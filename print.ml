64c61a508f4c3e4dab9493ccc3295e28bb0b3c01
Matthew Pereira <mbpereira49@gmail.com>
2021-09-11 19:38:54 -0400

    whoops, _now_ cleaned up printing

--- print.ml
+++ print.ml
@@ -2,51 +2,26 @@ open Types
 open Printf
 
 let print_var (v: var): string = 
-  Printf.sprintf "#(%d)" v;;
+  sprintf "#(%d)" v;;
 
-let rec print_term_list (t: term list): string =
-  match t with
-  | [] -> ""
-  | hd::tl -> 
-      let hd_string = print_term hd in
-      let tl_string = print_term_list tl in
-      Printf.sprintf "%s %s" hd_string tl_string
-and print_term (t: term): string =
+let print_list (l: 'a list) (f: ('a -> string)) (sep: string): string =
+  let string_l = List.map f l in
+  String.concat sep string_l
+
+let rec print_term (t: term): string = 
   match t with
   | Empty -> ""
   | Var v -> print_var v
-  | Val v -> Printf.sprintf "%d" v
-  | List t -> Printf.sprintf "(%s)" (print_term_list t);;
-
-let rec print_substitution_helper (s: substitution) = 
-  match s with
-  | [] -> ""
-  | (v, t)::tl -> Printf.sprintf "%s -> %s, %s" (print_var v) (print_term t) (print_substitution_helper tl);;
+  | Val v -> sprintf "%d" v
+  | List t -> sprintf "(%s)" (print_list t print_term " ");; 
 
 let print_substitution (s: substitution) = 
-  Printf.sprintf "[%s]" (print_substitution_helper s);;
+  let format_mapping (v, t) = sprintf "%s -> %s" (print_var v) (print_term t) in 
+  Printf.sprintf "[%s]" (print_list s format_mapping ", ");;
 
 let print_state ((s, c): state): string =
   Printf.sprintf "(%s, %d)" (print_substitution s) c;;
 
-let rec print_stream_helper (str: stream): string =
-  match str with
-  | [] -> ""
-  | st::tl -> Printf.sprintf "%s, %s" (print_state st) (print_stream_helper tl);;
-
 let print_stream (str: stream) =
-  let repr = Printf.sprintf "[%s]\n" (print_stream_helper str) in
+  let repr = Printf.sprintf "[%s]\n" (print_list str print_state ", ") in
   print_string repr;;
-
-(*let print_list (l: 'a list) (f: 'a -> string) (start: string) (end: string) (sep: string): string =
-  let l_string = List.map f l in
-  let rec helper (l : string list): string = 
-    match l with
-    | [] -> ""
-    | [a] -> a
-    | hd::tl -> *)
-
-let print_list (l: 'a list) (f: 'a -> string) (start: string) (end: string) (sep: string): string =
-  let string_l = List.map f l in
-  let base = String.concat sep string_l in
-  start ^ base ^ end;;
